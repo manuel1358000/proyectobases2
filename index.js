94253fe3fddb40ae67388c0f9467432eaf734fd4
manuel1358000 <manuel13580@gmail.com>
2019-09-24 10:44:11 -0600

    Se agrego el template para el desarrollo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 var request=require('request');
 
 app.get('/', function (req, res) {
-    res.send("hola mundo");
+    res.sendFile(path.join(__dirname+'/src/template/index.html'));
 });
-//servicio que se consume para dar el aviso al piloto redirige al servicio de piloto
 
 app.listen(3000, () => {
  console.log("Servidor en la direccion 127.0.0.1:3000");
