53e7a43a647019b15c8fdc49195df188a63eba42
manuel1358000 <manuel13580@gmail.com>
2019-09-25 23:54:54 -0600

    Se agrego la conexion a la base de datos

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const path = require('path');
 var request=require('request');
+var conexion=require('./src/js/conexiondb.js')
+
 
 app.get('/', function (req, res) {
+	conexion.consulta('select * from help');
     res.sendFile(path.join(__dirname+'/src/template/index.html'));
 });
 
 app.listen(3000, () => {
  console.log("Servidor en la direccion 127.0.0.1:3000");
 });
-
---
+++ src/js/conexiondb.js
@@ -0,0 +1,24 @@
+
+let conexion;
+var oracledb = require('oracledb');
+
+
+function consulta(query){
+	conexion=oracledb.getConnection(
+	{
+    	user          : "system",
+    	password      : "bases2",
+    	connectString : "localhost/XE"
+  	},
+  	function(err, connection){
+    if (err) { console.error(err); return; }
+    	connection.execute(
+      		query,
+      		function(err, result){
+        		if (err) { console.error(err); return; }
+        			console.log(result.rows);
+      		});
+  	});
+}
+
+module.exports.consulta = consulta;
