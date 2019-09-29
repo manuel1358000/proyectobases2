965512aa40d1ab1f25c96b155353953307d98702
CrisAlva25 <crisalva5@gmail.com>
2019-09-29 17:11:55 -0600

    Mas cambios a bancos

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const path = require('path');
 var request=require('request');
 var conexion=require('./src/js/conexiondb.js')
 
+//Archivos de bancos
+var bancos = require('./src/Bancos/Bancos.js')
+
 
 app.get('/', function (req, res) {
-	conexion.consulta('select * from help');
+	//conexion.consulta('select * from help');
     res.sendFile(path.join(__dirname+'/src/template/index.html'));
 });
 
