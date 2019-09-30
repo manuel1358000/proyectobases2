6f1b598dd8d26d512489cbebc5cf1746a84df74e
skrillfer <rluis4490@gmail.com>
2019-09-30 04:47:00 -0400

    integrated socket io

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+
 const express = require("express");
 const app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 const path = require('path');
-var request=require('request');
 var conexion=require('./src/js/conexiondb.js')
+
 
 app.use(express.static('src')); //Serves resources from public folder
 
 app.get('/', function (req, res) {
-	conexion.consulta('select * from help');
+	//conexion.consulta('select * from help');
     res.sendFile(path.join(__dirname+'/src/template/index.html'));
 });
 
-app.listen(3000, () => {
- console.log("Servidor en la direccion 127.0.0.1:3000");
+io.on('connection', function(socket) {
+    /*socket.on('new-message', function(data) {
+        messages.push(data);
+
+        io.sockets.emit('messages', messages);
+    });*/
 });
+
+server.listen(3000, function() {
+	console.log('Servidor corriendo en http://localhost:3000');
+});
+
+
