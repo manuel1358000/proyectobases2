1655e035cd4e5a9a3416838a9caf38c38cd4ba2d
Cristian David Alvarado Lopez <crisalva5@gmail.com>
2019-10-08 01:33:53 -0600

    Create DepositoBancario.sql
    Empezamos plsql del deposito

---
+++ PLSQL/Cristian/DepositoBancario.sql
@@ -0,0 +1,11 @@
+--Procedimiento para deposito bancario
+CREATE OR REPLACE deposito_bancario_transac (
+    cuenta_depositar NUMBER,
+    cantidad_depositar NUMBER,
+)
+IS
+BEGIN
+    
+
+
+END deposito_bancario_transac;
